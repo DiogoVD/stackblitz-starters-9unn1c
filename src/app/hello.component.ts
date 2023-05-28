324535ccc782eed543c7d22f5d7df69c1222c7f0
DiogoVD <diogovdamascena@gmail.com>
2023-05-28 08:53:19 -0300

    4 files modified, 1 added

---
+++ src/app/hello.component.ts
@@ -0,0 +1,9 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'helloApp',
+  templateUrl: 'hello.component.html',
+  styleUrls: ['hello.component.css'],
+  standalone: true,
+})
+export class helloApp {}
